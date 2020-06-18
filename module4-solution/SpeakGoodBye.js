8d5b16e73cedc3116185c01b6b782ad1d399a1ab
pavankumarabd17 <pavankumargadad17@gmail.com>
2020-06-18 17:55:34 +0530

    changes done

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -17,7 +17,7 @@ byeSpeaker.speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak=function(name) {
-  console.log(byeSpeaker.speakWord + " " + name);
+  console.log(byeSpeaker.sspeakWord + " " + name);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
